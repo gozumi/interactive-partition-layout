c4ba5fe46472fa0b8731887fab699bb93bd1fa42
Ian Anderson <ian@gozumi.com>
2018-05-11 10:17:03 +0100

    chore: Initial setup.

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,11 @@
 export function times10 (n: number) {
   return n * 10
 }
+
+/**
+ * Multiplies a given number by 10 and returns the result.
+ * @param n The number to be multiplied by 10
+ */
+export function times100 (n: number) {
+  return n * 100
+}
